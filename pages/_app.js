d5cc1301e64e55b6d45803fdba8d3a65876ed9d6
flowsonline <info@flowsonline.com>
2025-10-01 16:43:47 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,6 @@
+import '../styles/globals.css';
 
+// This is the root component that wraps all pages
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
