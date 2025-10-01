1700b07077e1aedd98a0cc8548c45366fb1b80c7
flowsonline <info@flowsonline.com>
2025-10-01 16:33:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.shotstack.io'], // Allow Shotstack images
+  },
+};
 
+module.exports = nextConfig;
